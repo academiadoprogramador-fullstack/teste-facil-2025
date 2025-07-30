4385c3b047176243e624dd18f2126b936475df1e
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-07-30 11:24:46 -0300

    Implementados testes de integração de Disciplina

---
+++ TesteFacil.Testes.Integracao/Compartilhado/DbContextFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TesteFacil.Infraestrutura.Orm.Compartilhado;
+
+namespace TesteFacil.Testes.Integracao.Compartilhado;
+
+public static class DbContextFactory
+{
+    private static readonly IConfiguration configuracao = CriarConfiguracao();
+
+    public static TesteFacilDbContext CriarDbContext()
+    {
+        var connectionString = configuracao["SQL_CONNECTION_STRING"];
+
+        var options = new DbContextOptionsBuilder<TesteFacilDbContext>()
+            .UseNpgsql(connectionString) 
+            .Options;
+
+        var dbContext = new TesteFacilDbContext(options);
+
+        dbContext.Database.EnsureDeleted();
+        dbContext.Database.EnsureCreated();
+
+        return dbContext;
+    }
+
+    private static IConfiguration CriarConfiguracao()
+    {
+        return new ConfigurationBuilder()
+            .AddUserSecrets(typeof(DbContextFactory).Assembly)
+            .AddEnvironmentVariables()
+            .Build();
+    }
+}
---
+++ TesteFacil.Testes.Integracao/ModuloDisciplina/RepositorioDisciplinaEmOrmTests.cs
@@ -0,0 +1,103 @@
+﻿using TesteFacil.Dominio.ModuloDisciplina;
+using TesteFacil.Infraestrutura.Orm.Compartilhado;
+using TesteFacil.Infraestrutura.Orm.ModuloDisciplina;
+using TesteFacil.Testes.Integracao.Compartilhado;
+
+namespace TesteFacil.Testes.Integracao.ModuloDisciplina;
+
+[TestClass]
+[TestCategory("Testes de Integração de Disciplina")]
+public sealed class RepositorioDisciplinaEmOrmTests
+{
+    private TesteFacilDbContext? dbContext;
+    private RepositorioDisciplinaEmOrm? repositorioDisciplina;
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        dbContext = DbContextFactory.CriarDbContext();
+
+        repositorioDisciplina = new RepositorioDisciplinaEmOrm(dbContext);
+    }
+
+    [TestMethod]
+    public void Deve_Cadastrar_Registro_Corretamente()
+    {
+        // Arrange (Configuração)
+        var entidade = new Disciplina("Ciências");
+
+        // Act (Ação)
+        repositorioDisciplina?.Cadastrar(entidade);
+
+        dbContext?.SaveChanges();
+
+        // Assert (Asserção)
+        var registroSelecionado = repositorioDisciplina?.SelecionarRegistroPorId(entidade.Id);
+
+        Assert.AreEqual(entidade, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Registro_Corretamente()
+    {
+        // Arrange (Configuração)
+        var entidade = new Disciplina("Ciências");
+        
+        repositorioDisciplina?.Cadastrar(entidade);
+        dbContext?.SaveChanges();
+
+        // Act (Ação)
+        var entidadeEditada = new Disciplina("Física");
+
+        var conseguiuEditar = repositorioDisciplina?.Editar(entidade.Id, entidadeEditada);
+        dbContext?.SaveChanges();
+
+        // Assert (Asserção)
+        var registroSelecionado = repositorioDisciplina?.SelecionarRegistroPorId(entidade.Id);
+
+        Assert.IsTrue(conseguiuEditar);
+        Assert.AreEqual(entidade, registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Registro_Corretamente()
+    {
+        // Arrange (Configuração)
+        var entidade = new Disciplina("Ciências");
+
+        repositorioDisciplina?.Cadastrar(entidade);
+        dbContext?.SaveChanges();
+
+        // Act (Ação)
+        var conseguiuExcluir = repositorioDisciplina?.Excluir(entidade.Id);
+        dbContext?.SaveChanges();
+
+        // Assert (Asserção)
+        var registroSelecionado = repositorioDisciplina?.SelecionarRegistroPorId(entidade.Id);
+
+        Assert.IsTrue(conseguiuExcluir);
+        Assert.IsNull(registroSelecionado);
+    }
+
+    [TestMethod]
+    public void Deve_Selecionar_Registros_Corretamente()
+    {
+        // Arrange (Configuração)
+        var entidade = new Disciplina("Ciências");
+        var entidade2 = new Disciplina("Artes");
+        var entidade3 = new Disciplina("Português");
+
+        List<Disciplina> entidades = [entidade, entidade2, entidade3];
+
+        repositorioDisciplina?.Cadastrar(entidade);
+        repositorioDisciplina?.Cadastrar(entidade2);
+        repositorioDisciplina?.Cadastrar(entidade3);
+        dbContext?.SaveChanges();
+
+        // Act (Ação)
+        var registros = repositorioDisciplina?.SelecionarRegistros();
+
+        // Assert (Asserção)
+        CollectionAssert.AreEqual(entidades, registros);
+    }
+}
