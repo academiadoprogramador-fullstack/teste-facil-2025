6f1bdfd98ef48a764fe6514bbe42a7219a040d05
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-08-11 12:50:31 -0300

    Adicionados testes de interface de disciplina

---
+++ TesteFacil.Testes.Interface/Compartilhado/TestFixture.cs
@@ -0,0 +1,70 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using TesteFacil.Infraestrutura.Orm.Compartilhado;
+using TesteFacil.Testes.Integracao.Compartilhado;
+
+namespace TesteFacil.Testes.Interface.Compartilhado;
+
+[TestClass]
+public abstract class TestFixture
+{
+    protected static IWebDriver? webDriver;
+    protected TesteFacilDbContext? dbContext;
+
+    protected static string enderecoBase = "https://localhost:7056";
+    private static string connectionString = "Host=localhost;Port=5432;Database=TesteFacilDb;Username=postgres;Password=YourStrongPassword";
+
+    //[AssemblyInitialize]
+    //public static void Setup(TestContext _)
+    //{
+    //    InicializarWebDriver();
+    //}
+
+    //[AssemblyCleanup]
+    //public static void Teardown()
+    //{
+    //    EncerrarWebDriver();
+    //}
+
+    [TestInitialize]
+    public void ConfigurarTestes()
+    {
+        dbContext = TesteFacilDbContextFactory.CriarDbContext(connectionString);
+
+        ConfigurarTabelas(dbContext);
+
+        InicializarWebDriver();
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        EncerrarWebDriver();
+    }
+
+    private static void InicializarWebDriver()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("--headless");
+
+        webDriver = new ChromeDriver(options);
+    }
+
+    private static void EncerrarWebDriver()
+    {
+        webDriver?.Quit();
+        webDriver?.Dispose();
+    }
+
+    private static void ConfigurarTabelas(TesteFacilDbContext dbContext)
+    {
+        dbContext.Database.EnsureCreated();
+
+        dbContext.Testes.RemoveRange(dbContext.Testes);
+        dbContext.Questoes.RemoveRange(dbContext.Questoes);
+        dbContext.Materias.RemoveRange(dbContext.Materias);
+        dbContext.Disciplinas.RemoveRange(dbContext.Disciplinas);
+
+        dbContext.SaveChanges();
+    }
+}
---
+++ TesteFacil.Testes.Interface/ModuloDisciplina/DisciplinaInterfaceTests.cs
@@ -0,0 +1,83 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using TesteFacil.Testes.Interface.Compartilhado;
+
+namespace TesteFacil.Testes.Interface.ModuloDisciplina;
+
+[TestClass]
+[TestCategory("Testes de Interface de Disciplina")]
+public sealed class DisciplinaInterfaceTests : TestFixture
+{
+    [TestMethod]
+    public void Deve_Cadastrar_Disciplina()
+    {
+        // Arrange
+        webDriver?.Navigate().GoToUrl(Path.Combine(enderecoBase, "disciplinas", "cadastrar"));
+
+        webDriver?.FindElement(By.Id("Nome")).SendKeys("Matemática");
+
+        // Act
+        webDriver?.FindElement(By.CssSelector("button[type=submit]")).Click();
+
+        // Assert
+        var elementosCard = webDriver?.FindElements(By.CssSelector(".card"));
+
+        Assert.AreEqual(1, elementosCard?.Count);
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Disciplina()
+    {
+        // Arrange
+        var wait = new WebDriverWait(webDriver!, TimeSpan.FromSeconds(5));
+
+        webDriver?.Navigate().GoToUrl(Path.Combine(enderecoBase, "disciplinas", "cadastrar"));
+
+        webDriver?.FindElement(By.Id("Nome")).SendKeys("Matemática");
+
+        webDriver?.FindElement(By.CssSelector("button[type=submit]")).Click();
+
+        wait.Until(d => d.FindElements(By.CssSelector(".card")).Count == 1);
+
+        webDriver?.FindElement(By.CssSelector(".card"))
+            .FindElement(By.CssSelector("a[title='Edição']")).Click();
+
+        webDriver?.FindElement(By.Id("Nome")).SendKeys(" Editada");
+
+        // Act
+        webDriver?.FindElement(By.CssSelector("button[type=submit]")).Click();
+
+        // Assert
+        wait.Until(d => d.FindElements(By.CssSelector(".card")).Count == 1);
+
+        Assert.IsTrue(webDriver?.PageSource.Contains("Matemática Editada"));
+    }
+
+    [TestMethod]
+    public void Deve_Excluir_Disciplina()
+    {
+        // Arrange
+        var wait = new WebDriverWait(webDriver!, TimeSpan.FromSeconds(5));
+
+        webDriver?.Navigate().GoToUrl(Path.Combine(enderecoBase, "disciplinas", "cadastrar"));
+
+        webDriver?.FindElement(By.Id("Nome")).SendKeys("Matemática");
+
+        webDriver?.FindElement(By.CssSelector("button[type=submit]")).Click();
+
+        wait.Until(d => d.FindElements(By.CssSelector(".card")).Count == 1);
+
+        webDriver?.FindElement(By.CssSelector(".card"))
+            .FindElement(By.CssSelector("a[title='Exclusão']")).Click();
+
+        // Act
+        webDriver?.FindElement(By.CssSelector("button[type=submit]")).Click();
+
+        // Assert
+        wait.Until(d => d.FindElements(By.CssSelector(".card")).Count == 0);
+
+        var elementosCard = webDriver?.FindElements(By.CssSelector(".card"));
+
+        Assert.AreEqual(0, elementosCard?.Count);
+    }
+}
