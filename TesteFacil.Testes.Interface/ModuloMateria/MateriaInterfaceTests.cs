faa584cfaf13b3b51546925ca49b0277826b927f
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-08-14 15:11:42 -0300

    Adicionados mais casos de teste

--- TesteFacil.Testes.Interface/ModuloMateria/MateriaInterfaceTests.cs
+++ TesteFacil.Testes.Interface/ModuloMateria/MateriaInterfaceTests.cs
@@ -1,4 +1,5 @@
 ﻿using TesteFacil.Testes.Interface.Compartilhado;
+using TesteFacil.Testes.Interface.ModuloDisciplina;
 
 namespace TesteFacil.Testes.Interface.ModuloMateria;
 
@@ -6,89 +7,92 @@ namespace TesteFacil.Testes.Interface.ModuloMateria;
 [TestCategory("Testes de Interface de Matéria")]
 public sealed class MateriaInterfaceTests : TestFixture
 {
-    //[TestMethod]
-    //public void Deve_Cadastrar_Materia_Corretamente()
-    //{
-    //    // Arrange
-    //    new DisciplinaIndexPageObject(driver!)
-    //       .IrPara(enderecoDriver)
-    //       .ClickCadastrar()
-    //       .PreencherNome("Matemática")
-    //       .Confirmar();
+    [TestMethod]
+    [Retry(3, MillisecondsDelayBetweenRetries = 500)]
+    public void Deve_Cadastrar_Materia_Corretamente()
+    {
+        // Arrange
+        new DisciplinaIndexPageObject(driver!)
+           .IrPara(enderecoBase!)
+           .ClickCadastrar()
+           .PreencherNome("Matemática")
+           .Confirmar();
 
-    //    // Act
-    //    var materiaIndex = new MateriaIndexPageObject(driver!)
-    //       .IrPara(enderecoDriver);
+        // Act
+        var materiaIndex = new MateriaIndexPageObject(driver!)
+           .IrPara(enderecoBase!);
 
-    //    materiaIndex
-    //        .ClickCadastrar()
-    //        .PreencherNome("Quatro Operações")
-    //        .SelecionarSerie("Segunda Série")
-    //        .SelecionarDisciplina("Matemática")
-    //        .Confirmar();
+        materiaIndex
+            .ClickCadastrar()
+            .PreencherNome("Quatro Operações")
+            .SelecionarSerie("Segunda Série")
+            .SelecionarDisciplina("Matemática")
+            .Confirmar();
 
-    //    // Assert
-    //    Assert.IsTrue(materiaIndex.ContemMateria("Quatro Operações"));
-    //}
+        // Assert
+        Assert.IsTrue(materiaIndex.ContemMateria("Quatro Operações"));
+    }
 
-    //[TestMethod]
-    //public void Deve_Editar_Materia_Corretamente()
-    //{
-    //    // Arrange
-    //    new DisciplinaIndexPageObject(driver!)
-    //       .IrPara(enderecoDriver)
-    //       .ClickCadastrar()
-    //       .PreencherNome("Matemática")
-    //       .Confirmar();
+    [TestMethod]
+    [Retry(3, MillisecondsDelayBetweenRetries = 500)]
+    public void Deve_Editar_Materia_Corretamente()
+    {
+        // Arrange
+        new DisciplinaIndexPageObject(driver!)
+           .IrPara(enderecoBase!)
+           .ClickCadastrar()
+           .PreencherNome("Matemática")
+           .Confirmar();
 
-    //    var materiaIndex = new MateriaIndexPageObject(driver!)
-    //        .IrPara(enderecoDriver);
+        var materiaIndex = new MateriaIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
 
-    //    materiaIndex
-    //        .ClickCadastrar()
-    //        .PreencherNome("Quatro Operações")
-    //        .SelecionarSerie("Segunda Série")
-    //        .SelecionarDisciplina("Matemática")
-    //        .Confirmar();
+        materiaIndex
+            .ClickCadastrar()
+            .PreencherNome("Quatro Operações")
+            .SelecionarSerie("Segunda Série")
+            .SelecionarDisciplina("Matemática")
+            .Confirmar();
 
-    //    // Act
-    //    materiaIndex
-    //        .ClickEditar()
-    //        .PreencherNome("Quatro Operações Editada")
-    //        .SelecionarSerie("Primeira Série")
-    //        .SelecionarDisciplina("Matemática")
-    //        .Confirmar();
+        // Act
+        materiaIndex
+            .ClickEditar()
+            .PreencherNome("Quatro Operações Editada")
+            .SelecionarSerie("Primeira Série")
+            .SelecionarDisciplina("Matemática")
+            .Confirmar();
 
-    //    // Assert
-    //    Assert.IsTrue(materiaIndex.ContemMateria("Quatro Operações Editada"));
-    //}
+        // Assert
+        Assert.IsTrue(materiaIndex.ContemMateria("Quatro Operações Editada"));
+    }
 
-    //[TestMethod]
-    //public void Deve_Excluir_Materia_Corretamente()
-    //{
-    //    // Arrange
-    //    new DisciplinaIndexPageObject(driver!)
-    //        .IrPara(enderecoDriver)
-    //        .ClickCadastrar()
-    //        .PreencherNome("Matemática")
-    //        .Confirmar();
+    [TestMethod]
+    [Retry(3, MillisecondsDelayBetweenRetries = 500)]
+    public void Deve_Excluir_Materia_Corretamente()
+    {
+        // Arrange
+        new DisciplinaIndexPageObject(driver!)
+            .IrPara(enderecoBase!)
+            .ClickCadastrar()
+            .PreencherNome("Matemática")
+            .Confirmar();
 
-    //    var materiaIndex = new MateriaIndexPageObject(driver!)
-    //        .IrPara(enderecoDriver);
+        var materiaIndex = new MateriaIndexPageObject(driver!)
+            .IrPara(enderecoBase!);
 
-    //    materiaIndex
-    //        .ClickCadastrar()
-    //        .PreencherNome("Quatro Operações")
-    //        .SelecionarSerie("Segunda Série")
-    //        .SelecionarDisciplina("Matemática")
-    //        .Confirmar();
-        
-    //    // Act
-    //    materiaIndex
-    //        .ClickExcluir()
-    //        .ConfirmarExclusao();
+        materiaIndex
+            .ClickCadastrar()
+            .PreencherNome("Quatro Operações")
+            .SelecionarSerie("Segunda Série")
+            .SelecionarDisciplina("Matemática")
+            .Confirmar();
 
-    //    // Assert
-    //    Assert.IsFalse(materiaIndex.ContemMateria("Quatro Operações"));
-    //}
+        // Act
+        materiaIndex
+            .ClickExcluir()
+            .ConfirmarExclusao();
+
+        // Assert
+        Assert.IsFalse(materiaIndex.ContemMateria("Quatro Operações"));
+    }
 }
